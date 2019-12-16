12dd3c68b923b518462bf54dda2a25f4ab4c535b
arthiatsr <arthiatsr@users.noreply.github.com>
2019-12-15 21:41:10 -0600

    Tried to use handlebar to render the scrapped data, results were sucessfully seen in network tab but not in browser. Hence used HTML

--- public/app.js
+++ public/app.js
@@ -1,23 +1,44 @@
-// When scrape new acticles is clicked
-$(".btn-newarticle").on("click", function() {
 
-    $.post("/scrape",function(data)
+$(document).ready(function(){
+
+$(".btn-newarticle").on("click", function(event) {
+    event.preventDefault();
+
+    $.get("/scrape",function(data)
     {
-        console.log("hi i am inside here",data)
-    
-    //    console.log("scrape")
-    });
 
-    $.getJSON("/articles", function(data){
-
-        console.log(data);
-        for (var i = 0; i<data.length; i++)
-        {
+        console.log("hi i am inside here",data);
+        for(var i = 0;i<data.length;i++){
+            // $("#articles").append("<p>" + data[i].title + "</p><button type='" +"button'"+"class='"+"btn btn-success'"+">Save Article</button>'");
+            // $("#articles").append("<p>" + data[i].desc + "</p>");
+            // $("#articles").append("<p>" + data[i].link + "</p>");
             $("#articles").append(
-                "<p data-id='"+data[i]._id + "'>" + data[i].title + "<br />" + data[i].desc + "<br />" + data[i].link + "</p>");
-
+            "<div class='"+"container'"+"><div class='"+"row table-primary border border-primary'"+"><div class='"+"col-10'"+"><a href='"+"https://www.nytimes.com"+data[i].link+"' target='"+"blank'"+"><h4>"+data[i].title+"</h4></a></div><div class='"+"col-2'"+"><button type='" +"button'"+"class='"+"btn btn-success btn-savearticle'"+">Save Article</button></div></div><div class='"+"row table-light border border-dark'"+"><div class='"+"col-sm'"+">"+data[i].desc+"</div></div><div class='"+"row'"+"><div class='"+"col-sm'"+"></div></div>")
         }
+            //     $("#articles").append("<p>" + articleArr[i].title + "</p>");
+        // res.render("articles",{articles: res});
+        // app.get("/articles",function(req,res){
+        //     res.render("user-listing", { listing: true });
+        //   });    
+        
+
     });
+    
+    // $(".btn-savearticle").on("click", function(event) {
+    //     event.preventDefault();
+
+    // });
+    //  $.get("/articles", function(data)
+    //  {
+
+    //      console.log(data);        
+    // // //     // for (var i = 0; i<data.length; i++)
+    // // //     // {
+    // // //     //     $("#articles").append(
+    // // //     //             "<p data-id='"+data[i]._id + "'>" + data[i].title + "<br />" + data[i].desc + "<br />" + data[i].link + "</p>");
+
+    // // //     // }
+    // });
 });
 
 // app.delete("/cleararticle",function(req, res) {
@@ -25,3 +46,5 @@ $(".btn-newarticle").on("click", function() {
 
 //     })
 // });
+
+})
--- server.js
+++ server.js
@@ -7,17 +7,34 @@ var PORT = process.env.PORT || 3000;
 var db = require("./models");
 var app = express();
 var request = require("request");
+var exphbs = require("express-handlebars");
+var path = require("path");
+app.use(express.static("public"));
+
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use(logger("dev"));
 // app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/populate";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
 
-app.post("/scrape", function(req, res) {
+
+//module.exports = function(app) {
+// app.get("/", function(req,res){
+//     res.sendFile(path.join(__dirname,"./index.html"));
+//     // res.render("home");
+
+//   });
+
+app.get("/scrape", function(req, res) 
+{
+    // var articlescrape = [];
 
     // request("https://www.foxnews.com/",function(err,response,html) {
     // $("article h2").each(function(i, element) {
@@ -32,43 +49,97 @@ app.post("/scrape", function(req, res) {
 
     request("https://www.nytimes.com/section/us",function(err,response,html) {
     var $ = cheerio.load(html);
+    var articleArr = [];
 
         $("li.css-ye6x8s").each(function(i,element){
         var result = {};
         result.title = $(this).find("h2").text();
         result.desc = $(this).find("p").text();
         result.link = $(this).find("a").attr("href");
-
+        articleArr.push(result);
 
         db.Article.create(result)
             .then(function(dbArticles) {
-                console.log("dbconnection succeful", dbArticles);
+                console.log("dbconnection succeful");
                 // res.json(dbArticles);
+                // const ArticleObj = {
+                // articleArray: articlListing
+                
+                
+                // res.render("articles", ArticleObj);
 
             })
             .catch(function(err) {
                  throw err;
             });
         });
+        console.log("articleArr====",articleArr)
+        // const articleObj = {
+        //     // listing: true,
+        //     articleArray: articleArr}
+        // res.render("articles", articleObj);
+            //     const articleArray = {
+            // // listing: true,
+            // title: articleArr}
+        // for(var i = 0;i<articleArr.length;i++){
+        //     $("#articles").append("<p>" + articleArr[i].title + "</p>");
 
-    // res.send("result");
+        //     // var html = "<p>";
+        //     // html += articleArr[i].title;
+        //     // html += "</p><p>"
+        //     // html += articleArr[i].desc;
+        //     // html += "</p><p>"
+        //     // html += articleArr[i].link;
+        //     // html += "</p>"
+
+        // }
+        // console.log(html);
+        // var container = $("#articles");
+        // $("#articles").text(html) ;
+
+     //   res.sendFile(path.join(__dirname,"/public/index.html"));
+
+        res.send(articleArr);
+//        res.render("articles", {articles: articleArr});
+
+    //res.send("result");
+    // articlescrape = articleArr;
+    // console.log("####",dbArticles);
 
     });
 });
 
-app.get("/articles", function(req, res) {
-    db.Article.find({})
-        .then(function(dbArticle) {
-        res.json(dbArticle);
-        console.log("%%%%%",dbArticle);
+    //console.log("^^^^^^",dbArticles);
 
-        })
-        .catch(function(err) {
-        res.json(err);
-        });
-});
+    // res.render("articles",{articles: articlescrape});
+    // app.get("/articles", function(req, res) 
+    // {
+    //     db.Article.find({})
+    //         .then(function(articlListing) 
+    //         {
+    //             // const ArticleObj = {
+    //                 // articleArray: articlListing
+    //                 const {title, desc, link} = articlListing.dataValues
+    //                 res.render("articles",{
+    //                     title,
+    //                     desc,
+    //                     link
+    //                 });
+                
+    //             // res.render("articles", ArticleObj);
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-app.listen(PORT, function() {
+    //         })
+    //         .catch(function(err)
+    //         {
+    //             console.log("There is a db query err");
+    //             res.status(500).end();
+    //         });        
+    // });
+
+
+
+//};                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+app.listen(PORT, function() 
+{
     console.log("App running on port " + PORT + "!");
-  });
+});
